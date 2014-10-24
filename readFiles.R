f3632d22509c4521d5ff7121936d4d9c403b1ed7
j-hagedorn <josh.hagedorn@gmail.com>
2014-10-24 00:25:01 -0400

    Init openBH proj; import AHRF

---
+++ readFiles.R
@@ -0,0 +1,26 @@
+
+
+# Area Health Resources Files (AHRF)
+# http://ahrf.hrsa.gov/
+
+# National Survey on Drug Use and Health
+# https://github.com/ajdamico/usgsd/tree/master/National%20Survey%20on%20Drug%20Use%20and%20Health
+
+# BRFSS
+# 
+
+# National Data Archive on Child Abuse and Neglect
+# http://www.ndacan.cornell.edu/datasets/datasets-list.cfm
+
+
+# Uniform Crime Reporting 
+# http://www.icpsr.umich.edu/icpsrweb/content/NACJD/guides/ucr.html
+
+# HUD Homelessness Exchange
+# https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+
+# Food Environment Atlas
+# http://www.ers.usda.gov/data-products/food-environment-atlas/data-access-and-documentation-downloads.aspx#.VEXQQPnF-mE
+
+# CDC Cause of Death
+# http://wonder.cdc.gov/mcd-icd10.html
