558b56c1f195641376c824208d593fc15eacb05c
j-hagedorn <josh.hagedorn@gmail.com>
2014-12-04 00:20:27 -0500

    Update read AFC file

--- readAFC.R
+++ readAFC.R
@@ -10,3 +10,24 @@ afc <- read.csv("http://www.dleg.state.mi.us/fhs/brs/txt/afc_sw.txt", header = F
                               "LicenseeAddress","LicenseeSuppAddress","LicenseeCity","LicenseeState",
                               "LicenseeZip","LicenseePhone","LicenseeStatus"))
 
+# Recode Facility Type
+library(car)
+afc$TypeDesc <- recode(afc$FacilityType,
+                       "'AF' = 'Family Home';
+                       'AS' = 'Small Group';
+                       'AM' = 'Medium Group';
+                       'AL' = 'Large Group';
+                       'AG' = 'Congregate';
+                       'AI' = 'County Infirmary';
+                       'AH' = 'Home for the Aged'")
+
+# Dates as date class
+library(lubridate)
+afc$Effective <- ymd(afc$Effective)
+afc$Expiration <- ymd(afc$Expiration)
+
+# Log download date
+afc$Downloaded <- Sys.Date()
+
+# Write to file
+write.csv(afc, file = "data/afc.csv")
