59223060312eb85c2520cbbb69606e2e0017a7f2
Joshh <josh.hagedorn@gmail.com>
2016-06-07 09:04:43 -0400

    Init global.R script

---
+++ misuddr-app/global.R
@@ -0,0 +1,26 @@
+## global.R ##
+
+library(shinythemes)
+library(dplyr)
+library(magrittr)
+library(tidyr)
+library(xts)
+library(car)
+library(scales)
+library(lubridate)
+library(plotly)
+library(ggplot2)
+library(dygraphs)
+library(RColorBrewer)
+
+
+# Get data
+
+drug_death <- read.csv("data/drug_death.csv")
+
+# Extract undup totals per year for calculating grouped rates
+totals <-
+drug_death %>%
+  group_by(PIHPname,CMHSP,county,year) %>%
+  summarize(TotalPop = max(TotalPop, na.rm = T)) %>%
+  ungroup() 
