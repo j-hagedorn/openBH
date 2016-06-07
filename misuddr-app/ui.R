971a0c3a62f3950fcf916da7399374ff483bb857
Joshh <josh.hagedorn@gmail.com>
2016-06-07 18:57:01 -0400

    Add descriptive text, directions

--- misuddr-app/ui.R
+++ misuddr-app/ui.R
@@ -7,6 +7,11 @@ dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem(
+        "The epidemic...", 
+        tabName = "epidemic", 
+        icon = icon("heartbeat")
+      ),
+      menuItem(
         "Compare", 
         tabName = "compare", 
         icon = icon("bar-chart")
@@ -35,6 +40,87 @@ dashboardPage(
   ),
   dashboardBody(
     tabItems(
+      tabItem(
+        tabName = "epidemic",
+        fluidRow(
+          column(
+            width = 12,
+            box(
+              title = "A National Epidemic", 
+              color = "black",
+              collapsible = T, 
+              width = NULL,
+              p(
+                "According to the United States' ",
+                a(href = "http://www.cdc.gov/drugoverdose/epidemic/index.html",
+                  "Centers for Disease Control"),
+                ", more people died from drug overdoses in 2014 than any other 
+                year on record. Most drug overdose deaths (>60%) involve an 
+                opioid, primarily prescription pain relievers and heroin. 
+                And the problem is growing.  Since 1999, the rate of overdose 
+                deaths involving opioids (including prescription opioid pain 
+                relievers and heroin) has nearly quadrupled."
+              )
+            ),
+            box(
+              title = "Local Focus", 
+              color = "black",
+              collapsible = T, 
+              width = NULL,
+              p(
+                "Michigan was one of a handful of states to show a statistically 
+                significant increase in the rate of drug overdoses between 2013 
+                and 2014 (",
+                em("Source: "),
+                a(href = "http://www.cdc.gov/drugoverdose/data/statedeaths.html",
+                  "CDC"), ")."
+              ),
+              p(
+                "In order to better understand the nuanced local variations 
+                influencing this statewide trend, we used data from the 
+                Michigan Substance Use Data Repository (", 
+                a(href = "http://mi-suddr.com/about/",
+                  "MI-SUDDR"),
+                ") to allow for analysis by county for the years ",
+                paste0(min(drug_death$year), " through ",max(drug_death$year)),"."
+              ),
+              p(
+                "Results can also be grouped by Prepaid Inpatient Health Plan 
+                (PIHP), since the PIHP acts as the Substance Use Disorder 
+                Coordinating Agency to manage services for people with 
+                substance use disorders.  Access to substance use services is 
+                also coordinated through Community Mental Health (CMH) service 
+                providers, and the regional data can be grouped by CMH as well."
+              )
+            ),
+            box(
+              title = "Getting Started", 
+              color = "black",
+              collapsible = T, 
+              width = NULL,
+              p(
+                "Select the ", em("Compare"), " and ", em("Trend"), 
+                " tabs on the menu to the left in order to start learning 
+                about how this epidemic is affecting communities throughout 
+                Michigan."
+              ),
+              p(
+                "The ", em("Group By"), " dropdown allows you to select how you 
+                want to aggregate the analysis: by County, CMHSP, or PIHP."
+              ),
+              p(
+                "You can also ", em("Select PIHP")," and ", em("Select CMH"), 
+                "in order to focus in on a specific regional grouping of 
+                counties ",
+                a(href = "https://www.macmhb.org/sites/default/files/attachments/files/COUNTYMAPBYNewPIHP2016.pdf",
+                  "by PIHP"), " or ",
+                a(href = "http://www.michigan.gov/mdhhs/0,5885,7-339-71550_2941_4868_4899-178824--,00.html",
+                  "by CMH")
+              )
+            )
+          )
+        )
+      ),
       tabItem(
         tabName = "compare",
         fluidRow(
@@ -68,7 +154,26 @@ dashboardPage(
                     sep = "", 
                     dragRange = T
                   ),
-                  plotlyOutput("death_bar")
+                  plotlyOutput("death_bar"),
+                  br(),
+                  h5(textOutput("define_pareto"))
+                ),
+                tabPanel(
+                  "About",
+                  h4("Pareto Charts"),
+                  p(
+                    a(href = "https://en.wikipedia.org/wiki/Pareto_chart",
+                      "Pareto charts"),
+                    " are a type of mixed bar-and-line diagram which arranges 
+                    various factors according to the magnitude of their impact. 
+                    The chart is a visual display of the ", 
+                    em("Pareto Principle"), 
+                    ", which states that a small number of members of a group 
+                    often account for the majority of the effect. Using a 
+                    Pareto chart can help teams to focus on factors that have 
+                    the greatest effect and communicate the reason for their 
+                    focus."
+                  )
                 )
               ),
               tabBox(
@@ -86,7 +191,70 @@ dashboardPage(
                     sep = "", 
                     dragRange = T
                   ),
-                  plotlyOutput("death_rate")
+                  plotlyOutput("death_rate"),
+                  br(),
+                  h5(textOutput("define_rate"))
+                ),
+                tabPanel(
+                  "About",
+                  h4("Calculating Rates"),
+                  p(
+                    "Calculating rates is a common way to allow for comparisons 
+                    across geographic areas with varying population sizes.  
+                    Per 100,000 rates are common when reporting deaths.  
+                    While there may or may not be 100,000 residents in the 
+                    county under review, multiplying the result by 100,000 
+                    makes that rate comparable with counties which have more 
+                    or less than 100,000 residents.  The formula used is ",
+                    code("Number of deaths / Total Population * 100,000"),"."
+                  ),
+                  h4("Population Data"),
+                  p(
+                    "Population estimates used in rate calculations are drawn 
+                    from the ",
+                    a(href = "http://www.census.gov/data/developers/data-sets/acs-survey-5-year-data.html",
+                      "Census API"),
+                    " and use American Community Survey (ACS) 5-year Estimates ",
+                    a(href = "https://www.census.gov/programs-surveys/acs/guidance/estimates.html",
+                      "to support increased accuracy.")
+                  ),
+                  p(
+                    "Currently, only years from 2009 onward are available for 
+                    download from the Census API."
+                  )
+                )
+              )
+            )
+          )
+        )
+      ),
+      tabItem(
+        tabName = "trend",
+        fluidRow(
+          column(
+            width = 12,
+            box(
+              title = "Trending", 
+              color = "black",
+              collapsible = F, 
+              width = NULL,
+              tabBox(
+                width = NULL,
+                tabPanel(
+                  "By Cause",
+                  plotlyOutput("line_cause")
+                ),
+                tabPanel(
+                  "By Group",
+                  selectInput(
+                    "cause_line",
+                    label = "Select cause of death:",
+                    choices = c("Heroin overdoses",
+                                "Opioid overdoses",
+                                "All overdose deaths"), 
+                    selected = "Opioid overdoses"
+                  ),
+                  plotlyOutput("line_group")
                 )
               )
             )
