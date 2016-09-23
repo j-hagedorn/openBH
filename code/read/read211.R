3896b55aa17069085373ccb01f706739dfb3c53b
Joshh <josh.hagedorn@gmail.com>
2016-09-23 10:21:48 -0400

    Add init geocoding script

--- code/read/read211.R
+++ code/read/read211.R
@@ -29,6 +29,7 @@ names(kent_211)[19] <- "phone_5"
 # Function to capitalize first letter
 
 simpleCap <- function(x) {
+  x <- tolower(x)
   s <- strsplit(x, " ")[[1]]
   paste(toupper(substring(s, 1,1)), substring(s, 2),
         sep="", collapse=" ")
@@ -36,7 +37,8 @@ simpleCap <- function(x) {
 
 # Clean data
 
-kent_211 %<>%
+tst <-
+kent_211 %>%
   mutate(service_term = as.factor(service_term),
          service_phone_1 = as.character(service_phone_1),
          service_phone_2 = as.character(service_phone_2),
@@ -51,9 +53,58 @@ kent_211 %<>%
          address = paste0(address_1," ",
                           ifelse(is.na(address_2) == T,
                                  "",address_2),", ",
-                          city,", ",state,", ",zip))
+                          city,", ",state,", ",zip)) %>%
+  # Define address variable for use in function
+  mutate(address = paste0(toupper(address_1), ", ",
+                          toupper(address_2), ", ",
+                          toupper(city), ", ",
+                          toupper(state), ", ",
+                          zip))
 
 
+
+###############################################
+
+# Add lat/long of addresses
+library(ggmap)
+
+
+
+
+#initialise a dataframe to hold the results
+geocoded <- data.frame()
+# find out where to start in the address list (if the script was interrupted before):
+# startindex <- 2527
+#if a temp file exists - load it up and count the rows!
+tempfilename <- paste0('addresses_temp_geocoded.rds')
+if (file.exists(tempfilename)) {
+  print("Found temp file - resuming from index:")
+  geocoded <- readRDS(tempfilename)
+  startindex <- nrow(geocoded)
+  print(startindex)
+}
+
+# Start the geocoding process - address by address. geocode() function takes care of query speed limit.
+for (ii in seq(1904, length(geo$address))) {
+  print(paste("Working on index", ii, "of", length(geo$address)))
+  #query the google geocoder - this will pause here if we are over the limit.
+  result = geocode(location = geo$address[ii],
+                   output = "latlona", 
+                   source = "google") 
+  print(result$status)     
+  result$index <- ii
+  #append the answer to the results file.
+  geocoded <- rbind(geocoded, result)
+  #save temporary results as we are going along
+  saveRDS(geocoded, tempfilename)
+}
+# errored on index 334, 408, 546, 720
+
+#now we add the latitude and longitude to the main data
+geo$lat <- geocoded$lat
+geo$long <- geocoded$long
+geo$accuracy <- geocoded$accuracy
+geo$formatted_address <- geocoded$formatted_address
 
 # Aggregate
 
